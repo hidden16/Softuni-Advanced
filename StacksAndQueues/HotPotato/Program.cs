21bf65f3cb52a71b80f05759ff7b14d428491e1d
Иксан <iksan168@abv.bg>
2021-12-15 15:33:35 +0200

    StacksAndQueues-Solved

---
+++ StacksAndQueues/HotPotato/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HotPotato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            Queue<string> queue = new Queue<string>(input);
+            int n = int.Parse(Console.ReadLine());
+            while (queue.Count > 1)
+            {
+                for (int i = 0; i < n - 1; i++)
+                {
+                    var player = queue.Dequeue();
+                    queue.Enqueue(player);
+                }
+                Console.WriteLine($"Removed {queue.Dequeue()}");
+            }
+            Console.WriteLine($"Last is {queue.Dequeue()}");
+        }
+    }
+}
