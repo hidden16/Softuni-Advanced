7903b78cb2fdf58516991a2fc178361af9b8cd83
Иксан <iksan168@abv.bg>
2021-12-15 15:38:59 +0200

    StacksAndQueues-Solved

---
+++ StacksAndQueues/TrafficJam/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TrafficJam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pass = int.Parse(Console.ReadLine());
+            Queue<string> queue = new Queue<string>();
+            var input = Console.ReadLine();
+            int counter = 0;
+            while (input != "end")
+            {
+                if (input != "green")
+                {
+                    queue.Enqueue(input);
+                }
+                else
+                {
+                    for (int i = 0; i < pass; i++)
+                    {
+                        if (queue.Count == 0)
+                        {
+                            break;
+                        }
+                        Console.WriteLine($"{queue.Dequeue()} passed!");
+                        counter++;
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"{counter} cars passed the crossroads.");
+        }
+    }
+}
