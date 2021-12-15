8302917f65d6f4ae3dd954cec91ff643d1d476c1
Иксан <iksan168@abv.bg>
2021-12-15 14:53:45 +0200

    StacksAndQueues-Solved

---
+++ StacksAndQueues/ReverseStrings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReverseStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            Stack<string> st = new Stack<string>();
+            foreach (var item in input)
+            {
+                st.Push(item.ToString());
+            }
+            foreach (var item in st)
+            {
+                Console.Write(item);
+            }
+        }
+    }
+}
