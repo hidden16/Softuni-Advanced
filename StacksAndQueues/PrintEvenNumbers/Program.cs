bae661991edcd5806b2f315dfdc1f9d42a87bb22
Иксан <iksan168@abv.bg>
2021-12-15 15:26:51 +0200

    StacksAndQueues-Solved

---
+++ StacksAndQueues/PrintEvenNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrintEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            Queue<int> queue = new Queue<int>();
+            List<int> output = new List<int>();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                queue.Enqueue(int.Parse(numbers[i]));
+            }
+            while (queue.Count != 0)
+            {
+                var currNum = queue.Peek();
+                if (currNum % 2 == 1)
+                {
+                    queue.Dequeue();
+                }
+                else if (currNum % 2 == 0)
+                {
+                    output.Add(queue.Dequeue());
+                }
+            }
+            Console.WriteLine(string.Join(", ", output));
+        }
+    }
+}
