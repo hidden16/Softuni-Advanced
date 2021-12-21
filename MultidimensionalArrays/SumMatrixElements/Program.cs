7a7ad5a39ffe1bf123fa09648732196859060c55
Иксан <iksan168@abv.bg>
2021-12-21 12:14:58 +0200

    MultidimensionalArrays-Solved

---
+++ MultidimensionalArrays/SumMatrixElements/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace SumMatrixElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var matrixInput = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[,] matrix = new int[matrixInput[0], matrixInput[1]];
+            var sum = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                var tokens = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = tokens[j];
+                    sum += tokens[j];
+                }
+            }
+            Console.WriteLine(matrix.GetLength(0));
+            Console.WriteLine(matrix.GetLength(1));
+            Console.WriteLine(sum);
+        }
+    }
+}
