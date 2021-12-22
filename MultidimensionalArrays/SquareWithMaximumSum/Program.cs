5334bb3167367bdd287033f4005d9fdeb6da7b00
Иксан <iksan168@abv.bg>
2021-12-22 15:06:07 +0200

    MultidimensionalArrays-Solved

---
+++ MultidimensionalArrays/SquareWithMaximumSum/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace SquareWithMaximumSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dimensionInput = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[,] matrix = new int[dimensionInput[0], dimensionInput[1]];
+            var maxSum = int.MinValue;
+            var selectedRow = -1;
+            var selectedCol = -1;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                var input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = input[j];
+                    
+                }
+            }
+            for (int i = 0; i < matrix.GetLength(0) - 1; i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+                {
+                    int sum = matrix[i, j] + matrix[i, j + 1] + matrix[i + 1, j] + matrix[i + 1, j + 1];
+                    if (sum > maxSum)
+                    {
+                        maxSum = sum;
+                        selectedRow = i;
+                        selectedCol = j;
+                    }
+                }
+            }
+            Console.WriteLine($"{matrix[selectedRow, selectedCol]} {matrix[selectedRow, selectedCol + 1]}\n{matrix[selectedRow+1,selectedCol]} {matrix[selectedRow + 1, selectedCol + 1]}\n{maxSum}");
+        }
+    }
+}
