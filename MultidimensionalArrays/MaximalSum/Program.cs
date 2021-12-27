5f568f69f63f10b2a800b2e72c667c377ae30eae
Иксан <iksan168@abv.bg>
2021-12-27 18:32:45 +0200

    MultidimensionalArrays-Exercise-Solved

---
+++ MultidimensionalArrays/MaximalSum/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace MaximalSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimensions = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[,] matrix = new int[dimensions[0], dimensions[1]];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = input[j];
+                }
+            }
+            var maxSum = int.MinValue;
+            var sum = 0;
+            var targetRow = 0;
+            var targetCol = 0;
+            for (int row = 0; row < matrix.GetLength(0) - 2; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 2; col++)
+                {
+                    sum = matrix[row, col] + matrix[row, col + 1] + matrix[row, col + 2] + matrix[row + 1, col] + matrix[row + 1, col + 1] + matrix[row + 1, col + 2] + matrix[row + 2, col] + matrix[row + 2, col + 1] + matrix[row + 2, col + 2];
+                    if (sum > maxSum)
+                    {
+                        maxSum = sum;
+                        targetRow = row;
+                        targetCol = col;
+                    }
+                }
+            }
+            Console.WriteLine("Sum = " + maxSum);
+            for (int row = targetRow; row <= targetRow + 2; row++)
+            {
+                for (int col = targetCol; col <= targetCol + 2; col++)
+                {
+                    Console.Write($"{matrix[row,col]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
