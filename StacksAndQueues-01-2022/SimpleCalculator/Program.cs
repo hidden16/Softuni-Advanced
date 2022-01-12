d18cd5b1a786c6cfdb4a843dfec1721707f227d5
Иксан <iksan168@abv.bg>
2022-01-12 13:47:51 +0200

    StacksAndQueues-01-2022-Solved

---
+++ StacksAndQueues-01-2022/SimpleCalculator/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Stack<string> stack = new Stack<string>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                stack.Push(input[i]);
+            }
+            Stack<string> reversedStack = new Stack<string>();
+            while (stack.Count > 0)
+            {
+                reversedStack.Push(stack.Pop());
+            }
+            var sum = 0;
+            while (reversedStack.Count > 1)
+            {
+                if (int.Parse(reversedStack.Peek()) >= 0)
+                {
+                    sum = int.Parse(reversedStack.Pop());
+                    if (reversedStack.Peek() == "+")
+                    {
+                        reversedStack.Pop();
+                        sum += int.Parse(reversedStack.Pop());
+                    }
+                    else if (reversedStack.Peek() == "-")
+                    {
+                        reversedStack.Pop();
+                        sum -= int.Parse(reversedStack.Pop());
+                    }
+                    reversedStack.Push(sum.ToString());
+                }
+            }
+            Console.WriteLine(reversedStack.Pop()); ;
+        }
+    }
+}
