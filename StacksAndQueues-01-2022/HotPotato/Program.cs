3df57f27e12f1c83d9c538daae086a369e858a33
Иксан <iksan168@abv.bg>
2022-01-12 14:56:22 +0200

    StacksAndQueues-01-2022-Solved

---
+++ StacksAndQueues-01-2022/HotPotato/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HotPotato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split();
+            Queue<string> qu = new Queue<string>(input);
+            int tosses = int.Parse(Console.ReadLine());
+            var counter = 1;
+            while (qu.Count > 1)
+            {
+                if (counter == tosses)
+                {
+                    Console.WriteLine($"Removed {qu.Dequeue()}");
+                    counter = 1;
+                }
+                else
+                {
+                    qu.Enqueue(qu.Dequeue());
+                    counter++;
+                }
+            }
+            Console.WriteLine($"Last is {qu.Dequeue()}");
+        }
+    }
+}
