f2f0b405100a17f25f85fd78a3c29e24735595fa
Иксан <iksan168@abv.bg>
2022-01-12 13:59:42 +0200

    StacksAndQueues-01-2022-Solved

---
+++ StacksAndQueues-01-2022/MatchingBrackets/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MatchingBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            Stack<int> st = new Stack<int>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    st.Push(i);
+                }
+                else if (input[i] == ')')
+                {
+                    for (int j = st.Pop(); j <= i; j++)
+                    {
+                        Console.Write($"{input[j]}");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
