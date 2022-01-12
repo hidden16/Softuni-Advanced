f18caec4f000429f0c0e8bede04aef873378823c
Иксан <iksan168@abv.bg>
2022-01-12 14:31:04 +0200

    StacksAndQueues-01-2022-Solved

---
+++ StacksAndQueues-01-2022/Supermarket/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> qu = new Queue<string>();
+            var commands = Console.ReadLine();
+            while (commands!="End")
+            {
+                if (commands!= "Paid")
+                {
+                    qu.Enqueue(commands);
+                }
+                else
+                {
+                    while (qu.Count > 0)
+                    {
+                        Console.WriteLine(qu.Dequeue());
+                    }
+                }
+
+                commands = Console.ReadLine();
+            }
+            Console.WriteLine($"{qu.Count} people remaining.");
+        }
+    }
+}
