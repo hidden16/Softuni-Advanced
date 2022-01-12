9cdc10febe1e55dba2a0b1db9715bd4127048f0e
Иксан <iksan168@abv.bg>
2022-01-12 20:15:07 +0200

    StacksAndQueues-01-2022-Solved

---
+++ StacksAndQueues-01-2022/MaximumAndMinimumElement/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MaximumAndMinimumElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Stack<int> stack = new Stack<int>();
+            var stackSaver = new Stack<int>();
+            var minElement = int.MaxValue;
+            var maxElement = int.MinValue;
+            for (int i = 0; i < n; i++)
+            {
+                var commands = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                if (commands[0] == "1")
+                {
+                    var toPush = int.Parse(commands[1]);
+                    stack.Push(toPush);
+                }
+                else if (commands[0] == "2")
+                {
+                    stack.Pop();
+                }
+                else if (commands[0] == "3")
+                {
+                    stackSaver = new Stack<int>(stack);
+                    while (stack.Count > 0)
+                    {
+                        var numSaver = stack.Peek();
+                        if (stack.Pop() > maxElement)
+                        {
+                            maxElement = numSaver;
+                        }
+                    }
+                    if (stackSaver.Count > 0)
+                    {
+                        Console.WriteLine(maxElement);
+                    }
+                    stack = new Stack<int>(stackSaver);
+                }
+                else if (commands[0] == "4")
+                {
+                    stackSaver = new Stack<int>(stack);
+                    while (stack.Count > 0)
+                    {
+                        var numSaver = stack.Peek();
+                        if (stack.Pop() < minElement)
+                        {
+                            minElement = numSaver;
+                        }
+                    }
+                    if (stackSaver.Count > 0)
+                    {
+                        Console.WriteLine(minElement);
+                    }
+                    stack = new Stack<int>(stackSaver);
+                }
+            }
+            List<int> output = new List<int>();
+            while (stack.Count > 0)
+            {
+                output.Add(stack.Pop());
+            }
+            Console.WriteLine(string.Join(", ",output));
+        }
+    }
+}
