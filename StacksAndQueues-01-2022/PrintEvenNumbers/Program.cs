d10abc293768aa8b222b6adb87e2f329e19dd2f8
Иксан <iksan168@abv.bg>
2022-01-12 14:12:43 +0200

    StacksAndQueues-01-2022-Solved

---
+++ StacksAndQueues-01-2022/PrintEvenNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PrintEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> qu = new Queue<int>(input);
+            List<int> evenNums = new List<int>();
+            while (qu.Count > 0)
+            {
+                if (qu.Peek() % 2 == 0)
+                {
+                    evenNums.Add(qu.Dequeue());
+                }
+                else if (qu.Peek() % 2 == 1)
+                {
+                    qu.Dequeue();
+                }
+            }
+            Console.WriteLine(string.Join(", ",evenNums));
+        }
+    }
+}
