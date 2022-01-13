c81b76aaf5269adbab5365497374b43d0a17d98a
Иксан <iksan168@abv.bg>
2022-01-13 13:34:36 +0200

    StacksAndQueues-01-2022-Solved

---
+++ StacksAndQueues-01-2022/FashionBoutique/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FashionBoutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var clothes = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            Stack<int> stack = new Stack<int>(clothes);
+            var rackCap = int.Parse(Console.ReadLine());
+            var rackCounter = 1;
+            var sum = 0;
+            while (stack.Count>0)
+            {
+                if (stack.Peek() + sum <= rackCap)
+                {
+                    sum += stack.Pop();
+                }
+                else
+                {
+                    rackCounter++;
+                    sum = 0;
+                }
+            }
+            Console.WriteLine(rackCounter);
+        }
+    }
+}
