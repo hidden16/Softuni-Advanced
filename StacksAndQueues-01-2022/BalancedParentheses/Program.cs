49829d12d8ee27e94ae48ff18977d96db094ad42
Иксан <iksan168@abv.bg>
2022-01-13 17:17:38 +0200

    StacksAndQueues-01-2022-Solved

---
+++ StacksAndQueues-01-2022/BalancedParentheses/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BalancedParentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            Stack<char> stack = new Stack<char>();
+            char[] openingParentheses = new char[] { '(', '{', '[' };
+            bool isValid = true;
+            foreach (var item in input)
+            {
+                if (openingParentheses.Contains(item))
+                {
+                    stack.Push(item);
+                    continue;
+                }
+                if (stack.Count == 0)
+                {
+                    isValid = false;
+                    break;
+                }
+                if (stack.Peek() == '[' && item == ']')
+                {
+                    stack.Pop();
+                }
+                else if (stack.Peek() == '{' && item == '}')
+                {
+                    stack.Pop();
+                }
+                else if (stack.Peek() == '(' && item == ')')
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    isValid = false;
+                    break;
+                }
+            }
+            if (isValid)
+            {
+                Console.WriteLine($"YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
