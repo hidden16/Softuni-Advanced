b7b7314d2186223da1666619c7298d46d52f6c8f
Иксан <iksan168@abv.bg>
2022-01-14 17:29:16 +0200

    StacksAndQueues-01-2022-Solved

---
+++ StacksAndQueues-01-2022/KeyRevolver/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KeyRevolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var priceOfBullet = int.Parse(Console.ReadLine());
+            var magazineCapacity = int.Parse(Console.ReadLine());
+            Stack<int> bullets = new Stack<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            Queue<int> locks = new Queue<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            var safeValue = int.Parse(Console.ReadLine());
+            var bulletsLeft = magazineCapacity;
+            var bulletsCount = 0;
+            while (bullets.Count > 0 && locks.Count > 0)
+            {
+                var bullet = bullets.Pop();
+                var currentLock = locks.Peek();
+                bulletsLeft--;
+                bulletsCount++;
+                if (bullet <= currentLock)
+                {
+                    Console.WriteLine($"Bang!");
+                    locks.Dequeue();
+                }
+                else
+                {
+                    Console.WriteLine($"Ping!");
+                }
+                if (bulletsLeft == 0 && bullets.Count > 0)
+                {
+                    Console.WriteLine($"Reloading!");
+                    bulletsLeft = magazineCapacity;
+                }
+            }
+            if (locks.Count > 0)
+            {
+                Console.WriteLine($"Couldn't get through. Locks left: {locks.Count}");
+            }
+            else
+            {
+                safeValue -= (bulletsCount * priceOfBullet);
+                Console.WriteLine($"{bulletsLeft} bullets left. Earned ${safeValue}");
+            }
+        }
+    }
+}
