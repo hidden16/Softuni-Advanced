7cff0bad575e4256c622c5c7934ccf8c38147b96
Иксан <iksan168@abv.bg>
2022-01-15 12:42:08 +0200

    MultidimensionalArrays-01-2022-Solved

---
+++ MultidimensionalArrays-01-2022/PrimaryDiagonal/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace PrimaryDiagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var matrixSize = int.Parse(Console.ReadLine());
+            var matrix = new int[matrixSize, matrixSize];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+            var sum = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    sum += matrix[row, col];
+                    row++;
+                }
+                break;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ MultidimensionalArrays-01-2022/SymbolInMatrix/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace SymbolInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var matrixInfo = int.Parse(Console.ReadLine());
+            var matrix = new char[matrixInfo, matrixInfo];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var input = Console.ReadLine();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+            char checker = char.Parse(Console.ReadLine());
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(0); col++)
+                {
+                    if (matrix[row,col] == checker)
+                    {
+                        Console.WriteLine($"({row}, {col})");
+                        Environment.Exit(0);
+                    }
+                }
+            }
+            Console.WriteLine($"{checker} does not occur in the matrix");
+        }
+    }
+}
