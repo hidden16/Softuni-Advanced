397eec595546a5e53ed991517218f8b25cc45d10
Иксан <iksan168@abv.bg>
2022-01-15 13:34:57 +0200

    MultidimensionalArrays-01-2022-Solved

---
+++ MultidimensionalArrays-01-2022/DiagonalDifference/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace DiagonalDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dimensions = int.Parse(Console.ReadLine());
+            var matrix = new int[dimensions, dimensions];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+            var sumOne = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    sumOne += matrix[row, col];
+                    row++;
+                }
+                break;
+            }
+            var sumTwo = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = matrix.GetLength(1) - 1; col >= 0; col--)
+                {
+                    sumTwo += matrix[row, col];
+                    row++;
+                }
+                break;
+            }
+            Console.WriteLine($"{Math.Abs(sumOne-sumTwo)}");
+        }
+    }
+}
