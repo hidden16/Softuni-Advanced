8af4118fb07c735d9c17d315394e4a8cad6f4dc1
Иксан <iksan168@abv.bg>
2022-01-15 13:40:34 +0200

    MultidimensionalArrays-01-2022-Solved

---
+++ MultidimensionalArrays-01-2022/2x2SquaresInMatrix/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _2x2SquaresInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dimensions = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var matrix = new string[dimensions[0], dimensions[1]];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var input = Console.ReadLine().Split();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+            var counter = 0;
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    if (matrix[row, col] == matrix[row, col + 1] && matrix[row, col] == matrix[row + 1, col] && matrix[row, col] == matrix[row + 1, col + 1])
+                    {
+                        counter++;
+                    }
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
