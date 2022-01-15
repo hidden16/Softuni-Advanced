450e015738bab11a7d89f3d3c1f6996c8f028f38
Иксан <iksan168@abv.bg>
2022-01-15 13:23:45 +0200

    MultidimensionalArrays-01-2022-Solved

---
+++ MultidimensionalArrays-01-2022/PascalTriangle/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace PascalTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long n = long.Parse(Console.ReadLine());
+            long[][] matrix = new long[n][];
+            for (long row = 0; row < matrix.Length; row++)
+            {
+                matrix[row] = new long[row + 1];
+            }
+            for (long row = 0; row < matrix.Length; row++)
+            {
+                matrix[row][0] = 1;
+            }
+            for (long row = 1; row < matrix.Length; row++)
+            {
+                for (long col = 1; col < matrix[row].Length; col++)
+                {
+                    if (col > matrix[row-1].Length - 1)
+                    {
+                        matrix[row][col] = matrix[row - 1][col - 1];
+                    }
+                    else
+                    {
+                        matrix[row][col] = matrix[row - 1][col] + matrix[row - 1][col - 1];
+                    }
+                }
+            }
+            for (long row = 0; row < matrix.Length; row++)
+            {
+                for (long col = 0; col < matrix[row].Length; col++)
+                {
+                    Console.Write($"{matrix[row][col]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
