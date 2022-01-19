e8625890fcdbcbb86f7a3ce954182ec707d29633
Иксан <iksan168@abv.bg>
2022-01-19 12:52:04 +0200

    SetsAndDictionariesAdvanced-Solved

---
+++ SetsAndDictionariesAdvanced/CitiesByContinentAndCountry/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CitiesByContinentAndCountry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, List<string>>> continents = new Dictionary<string, Dictionary<string, List<string>>>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split();
+                var continent = input[0];
+                var country = input[1];
+                var city = input[2];
+                if (!continents.ContainsKey(continent))
+                {
+                    continents.Add(continent, new Dictionary<string, List<string>>());
+                    continents[continent].Add(country, new List<string> { city });
+                }
+                else
+                {
+                    if (!continents[continent].ContainsKey(country))
+                    {
+                        continents[continent].Add(country, new List<string> { city });
+                    }
+                    else
+                    {
+                        if (!continents[continent][country].Contains(city))
+                        {
+                            continents[continent][country].Add(city);
+                        }
+                    }
+                }
+            }
+            foreach (var item in continents)
+            {
+                Console.WriteLine($"{item.Key}:");
+                foreach (var country in item.Value)
+                {
+                    Console.WriteLine($"{country.Key} -> {string.Join(", ",country.Value)}");
+                }
+            }
+        }
+    }
+}
