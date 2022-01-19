101052cf8a0ea3df2d9e547cc1301bfcf4a88d16
Иксан <iksan168@abv.bg>
2022-01-19 12:25:53 +0200

    SetsAndDictionariesAdvanced-Solved

---
+++ SetsAndDictionariesAdvanced/Largest3Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Largest3Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            List<int> numbers = new List<int>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                numbers.Add(input[i]);
+            }
+            numbers = numbers.OrderByDescending(n => n).Take(3).ToList();
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                Console.Write($"{numbers[i]} ");
+            }
+        }
+    }
+}
