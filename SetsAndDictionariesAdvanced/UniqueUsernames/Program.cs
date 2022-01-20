77ec3934db8feee78b03448c90014181c283f66b
Иксан <iksan168@abv.bg>
2022-01-20 13:48:30 +0200

    SetsAndDictionariesAdvanced-Solved

---
+++ SetsAndDictionariesAdvanced/UniqueUsernames/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UniqueUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> usernames = new HashSet<string>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine();
+                usernames.Add(input);
+            }
+            foreach (var username in usernames)
+            {
+                Console.WriteLine(username);
+            }
+        }
+    }
+}
