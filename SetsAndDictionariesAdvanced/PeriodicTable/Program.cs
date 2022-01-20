47805088f2840fa50ddd907d7dbcf39364e1a30f
Иксан <iksan168@abv.bg>
2022-01-20 13:55:02 +0200

    SetsAndDicitionariesAdvanced-Solved

---
+++ SetsAndDictionariesAdvanced/PeriodicTable/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PeriodicTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> elements = new HashSet<string>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                for (int j = 0; j < input.Length; j++)
+                {
+                    elements.Add(input[j]);
+                }
+            }
+            foreach (var element in elements.OrderBy(x=>x))
+            {
+                Console.Write($"{element} ");
+            }
+        }
+    }
+}
