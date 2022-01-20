3936f61d9d8c110678cadd8a695c202238fc7c83
Иксан <iksan168@abv.bg>
2022-01-20 14:47:48 +0200

    SetsAndDictionariesAdvanced-Solved

---
+++ SetsAndDictionariesAdvanced/Wardrobe/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Wardrobe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, int>> wardrobe = new Dictionary<string, Dictionary<string, int>>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+                var color = input[0];
+                var items = input[1];
+                var itemSplitted = items.Split(",");
+                if (!wardrobe.ContainsKey(color))
+                {
+                    wardrobe.Add(color, new Dictionary<string, int>());
+                    for (int j = 0; j < itemSplitted.Length; j++)
+                    {
+                        wardrobe[color].Add(itemSplitted[j], 1);
+                    }
+                }
+                else
+                {
+                    for (int k = 0; k < itemSplitted.Length; k++)
+                    {
+                        if (!wardrobe[color].ContainsKey(itemSplitted[k]))
+                        {
+                            wardrobe[color].Add(itemSplitted[k], 1);
+                        }
+                        else
+                        {
+                            wardrobe[color][itemSplitted[k]]++;
+                        }
+                    }
+                }
+            }
+            var toLookFor = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            
+                foreach (var color in wardrobe)
+                {
+                    Console.WriteLine($"{color.Key} clothes:");
+                    foreach (var dress in color.Value)
+                    {
+                        if (toLookFor[0] == color.Key && toLookFor[1] == dress.Key)
+                        {
+                            Console.WriteLine($"* {dress.Key} - {dress.Value} (found!)");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"* {dress.Key} - {dress.Value}");
+                        }
+                    }
+                }
+            
+        }
+    }
+}
