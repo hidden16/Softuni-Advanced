5c2b16c8d745217477dfe5240b4c9653c8cfbd9a
Иксан <iksan168@abv.bg>
2022-01-20 14:26:02 +0200

    SetsAndDictionariesAdvanced-Solved

---
+++ SetsAndDictionariesAdvanced/CountSymbols/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountSymbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, int> counter = new Dictionary<char, int>();
+            var input = Console.ReadLine();
+            foreach (var letter in input)
+            {
+                if (!counter.ContainsKey(letter))
+                {
+                    counter.Add(letter, 1);
+                }
+                else
+                {
+                    counter[letter]++;
+                }
+            }
+            foreach (var item in counter.OrderBy(x=>x.Key))
+            {
+                Console.WriteLine($"{item.Key}: {item.Value} time/s");
+            }
+        }
+    }
+}
