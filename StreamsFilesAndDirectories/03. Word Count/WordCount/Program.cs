5147e5897f317bea8502b61a6200e799990d423f
Иксан <iksan168@abv.bg>
2022-01-25 13:22:54 +0200

    StreamsFilesAndDirectories-Solved

---
+++ StreamsFilesAndDirectories/03. Word Count/WordCount/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace WordCount
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var wordsFilePath = @"..\..\..\words.txt";
+            var textFilePath = @"..\..\..\text.txt";
+            var outputFilePath = @"..\..\..\output.txt";
+            WordCount.CalculateWordCounts(wordsFilePath, textFilePath, outputFilePath);
+        }
+    }
+    public class WordCount
+    {
+        public static void CalculateWordCounts(string wordsFilePath, string textFilePath, string outputFilePath)
+        {
+            Dictionary<string, int> words = new Dictionary<string, int>();
+            var wordsCheck = File.ReadAllText(wordsFilePath).Split();
+            foreach (var word in wordsCheck)
+            {
+                if (!words.ContainsKey(word))
+                {
+                    words.Add(word, 0);
+                }
+            }
+            using (StreamReader reader = new StreamReader(textFilePath))
+            {
+                var line = reader.ReadLine().ToLower();
+                while (line != null)
+                {
+                    var splittedLine = line.Split(new char[] { ' ', '.', ',', '-', '?', '!', ':', ';' }, StringSplitOptions.RemoveEmptyEntries);
+                    foreach (var word in wordsCheck)
+                    {
+                        foreach (var split in splittedLine)
+                        {
+                            if (word == split)
+                            {
+                                words[word]++;
+                            }
+                        }
+                    }
+                    line = reader.ReadLine();
+                    if (line != null)
+                    {
+                        line = line.ToLower();
+                    }
+                }
+                using (StreamWriter writer = new StreamWriter(outputFilePath))
+                {
+                    foreach (var word in words.OrderByDescending(x=>x.Value))
+                    {
+                        writer.WriteLine($"{word.Key} - {word.Value}");
+                    }
+                }
+            }
+        }
+    }
+
+}
