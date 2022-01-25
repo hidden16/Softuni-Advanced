80a81e62a15b7b28c5cbc30cf821aefe36333385
Иксан <iksan168@abv.bg>
2022-01-25 12:34:24 +0200

    StreamsFilesAndDirectories-Solved

---
+++ StreamsFilesAndDirectories/02. Line Numbers/LineNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace LineNumbers
+{
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\Files\input.txt";
+            string outputFilePath = @"..\..\..\Files\output.txt";
+
+            RewriteFileWithLineNumbers(inputFilePath, outputFilePath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            var index = 1;
+            using (StreamReader reader = new StreamReader(@"..\..\..\Files\input.txt"))
+            {
+                using (StreamWriter writer = new StreamWriter(@"..\..\..\Files\output.txt"))
+                {
+                    var line = reader.ReadLine();
+                    while (line != null)
+                    {
+                        writer.WriteLine(index + ". " + line, 0, 1);
+                        index++;
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+
+}
