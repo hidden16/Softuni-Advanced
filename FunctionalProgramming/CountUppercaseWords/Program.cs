b726f199074fddf0d3c261571fa12e6ecadff354
Иксан <iksan168@abv.bg>
2022-01-26 12:47:50 +0200

    FunctionalProgramming-Solved

---
+++ FunctionalProgramming/CountUppercaseWords/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace CountUppercaseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            Func<string, bool> uppercaseLetters = x => char.IsUpper(x[0]);
+            Console.WriteLine(string.Join("\n",input.Where(uppercaseLetters)));
+        }
+    }
+}
