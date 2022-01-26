f23acba8b8a8db503fa520619804f9162941ed6a
Иксан <iksan168@abv.bg>
2022-01-26 12:56:01 +0200

    FunctionalProgramming-Solved

---
+++ FunctionalProgramming/AddVAT/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AddVAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<decimal> numbers = Console.ReadLine().Split(", ",StringSplitOptions.RemoveEmptyEntries).Select(decimal.Parse).ToList();
+            numbers = numbers.Select(x => x * 1.2m).ToList();
+            numbers.ForEach(x => Console.WriteLine($"{x:f2}"));
+        }
+    }
+}
