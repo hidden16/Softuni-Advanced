6e6d97f2f37675f0cdf216ac826237530fa398e5
Иксан <iksan168@abv.bg>
2022-01-27 15:21:05 +0200

    FunctionalProgramming-Solved

---
+++ FunctionalProgramming/PredicateForNames/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace PredicateForNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            List<string> input = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).ToList();
+            Func<string, bool> filter = n => n.Length <= length ;
+            input = input.Where(filter).ToList();
+            Console.WriteLine(string.Join("\n",input));
+        }
+    }
+}
