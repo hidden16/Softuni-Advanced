e92e7001145655f70c212be267a3dce72eeddf04
Иксан <iksan168@abv.bg>
2022-01-27 15:27:43 +0200

    FunctionalProgramming-Solved

---
+++ FunctionalProgramming/ListOfPredicates/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListOfPredicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<int> numbers = new List<int>();
+            for (int i = 1; i <= n; i++)
+            {
+                numbers.Add(i);
+            }
+            int[] divisibles = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            for (int i = 0; i < divisibles.Length; i++)
+            {
+                Func<int, bool> filter = n => n % divisibles[i] == 0;
+                numbers = numbers.Where(filter).ToList();
+            }
+            Console.WriteLine(string.Join(" ",numbers));
+        }
+    }
+}
