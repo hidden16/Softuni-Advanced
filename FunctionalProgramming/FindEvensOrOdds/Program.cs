d56f1fed9e776f9e9b6c17e46357d4d3f40f4a9d
Иксан <iksan168@abv.bg>
2022-01-27 14:48:40 +0200

    FunctionalProgramming-Solved

---
+++ FunctionalProgramming/FindEvensOrOdds/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FindEvensOrOdds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numRange = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            List<int> numbers = new List<int>();
+            if (numRange[0] < numRange[1])
+            {
+                for (int i = numRange[0]; i <= numRange[1]; i++)
+                {
+                    numbers.Add(i);
+                }
+            }
+            else
+            {
+                for (int i = numRange[1]; i <= numRange[0]; i++)
+                {
+                    numbers.Add(i);
+                }
+            }
+            Predicate<int> evenChecker = n => n % 2 == 0 || n % 2 == -1;
+            Predicate<int> oddChecker = n => n % 2 == 1 || n % 2 == -1;
+            var input = Console.ReadLine();
+            switch (input)
+            {
+                case "odd":
+                    numbers = numbers.Where(w => oddChecker(w)).ToList();
+                    numbers.ForEach(w => Console.Write($"{w} "));
+                    break;
+                case "even":
+                    numbers = numbers.Where(w => evenChecker(w)).ToList();
+                    numbers.ForEach(w => Console.Write($"{w} "));
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
+}
