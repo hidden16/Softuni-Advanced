5e4e9106c96c96544b55f7a339acecddd35c1007
Иксан <iksan168@abv.bg>
2022-01-27 14:03:09 +0200

    FunctionalProgramming-Solved

---
+++ FunctionalProgramming/KnightsOfHonor/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace KnightsOfHonor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            Action<string> printer = input => Console.WriteLine($"Sir {input}");
+            for (int i = 0; i < input.Length; i++)
+            {
+                printer(input[i]);
+            }
+        }
+    }
+}
