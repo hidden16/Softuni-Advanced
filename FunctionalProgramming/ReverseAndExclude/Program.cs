1fb6329bd7369acb6d13594114288dd89a8f79e1
Иксан <iksan168@abv.bg>
2022-01-27 15:12:15 +0200

    FunctionalProgramming-Solved

---
+++ FunctionalProgramming/ReverseAndExclude/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseAndExclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int divider = int.Parse(Console.ReadLine());
+            Func<int, bool> filter = n => n % divider != 0;
+            var finalNumbers = numbers.Reverse().Where(filter);
+            Console.WriteLine(string.Join(" ",finalNumbers));
+        }
+    }
+}
