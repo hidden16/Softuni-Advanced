c0180f909aa10574e4ad64dc3995fedd33171445
Иксан <iksan168@abv.bg>
2022-01-27 14:01:24 +0200

    FunctionalProgramming-Solved

---
+++ FunctionalProgramming/ActionPoint/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ActionPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            Action<string> print = input => Console.WriteLine(input);
+            for (int i = 0; i < input.Length; i++)
+            {
+                print(input[i]);
+            }
+            
+        }
+    }
+}
