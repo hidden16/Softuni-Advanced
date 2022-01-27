609803cc0d77e057b2125ac7299ab2224e7cd9c7
Иксан <iksan168@abv.bg>
2022-01-27 14:53:10 +0200

    FunctionalProgramming-Solved

---
+++ FunctionalProgramming/AppliedArithmetics/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace AppliedArithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var commands = Console.ReadLine();
+            while (commands != "end")
+            {
+                switch (commands)
+                {
+                    case "add":
+                        numbers = numbers.Select(n => n + 1).ToArray();
+                        break;
+                    case "multiply":
+                        numbers = numbers.Select(n => n * 2).ToArray();
+                        break;
+                    case "subtract":
+                        numbers = numbers.Select(n => n - 1).ToArray();
+                        break;
+                    case "print":
+                        foreach (var number in numbers)
+                        {
+                            Console.Write($"{number} ");
+                        }
+                        Console.WriteLine();
+                        break;
+                    default:
+                        break;
+                }
+                commands = Console.ReadLine();
+            }
+        }
+    }
+}
