cf63ce942806a99d6542b48075b0116020debdb1
Иксан <iksan168@abv.bg>
2022-01-28 22:55:00 +0200

    DefiningClasses-ThirdExercise-Solved

--- DefiningClasses/Car/Program.cs
+++ DefiningClasses/Car/Program.cs
@@ -14,6 +14,15 @@ namespace CarManufacturer
             car.FuelConsumption = 200;
             car.Drive(2000);
             Console.WriteLine(car.WhoAmI());
+            //Third exercise -------
+            string make = Console.ReadLine();
+            string model = Console.ReadLine();
+            int year = int.Parse(Console.ReadLine());
+            double fuelQuantity = double.Parse(Console.ReadLine());
+            double fuelConsumption = double.Parse(Console.ReadLine());
+            Car firstCar = new Car();
+            Car secondCar = new Car(make, model, year);
+            Car thirdCar = new Car(make, model, year, fuelQuantity, fuelConsumption);
         }
     }
 }
