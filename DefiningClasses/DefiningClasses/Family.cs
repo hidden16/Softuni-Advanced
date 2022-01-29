932e26cc1e5a77d3b200f937c6c2ff6e7541ba73
Иксан <iksan168@abv.bg>
2022-01-29 16:40:36 +0200

    DefiningClasses-ExerciseThree-Solved

---
+++ DefiningClasses/DefiningClasses/Family.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        Dictionary<string, int> people = new Dictionary<string, int>();
+        public Dictionary<string,int> People { get { return people; }}
+        public void AddMember(Person member)
+        {
+            People.Add(member.Name, member.Age);
+        }
+        public void GetOldestMember()
+        {
+            var oldestMemberName = String.Empty;
+            var oldestMemberAge = int.MinValue;
+            foreach (var member in People)
+            {
+                if (member.Value > oldestMemberAge)
+                {
+                    oldestMemberName = member.Key;
+                    oldestMemberAge = member.Value;
+                }
+            }
+            Console.WriteLine($"{oldestMemberName} {oldestMemberAge}");
+        }
+    }
+}
--- DefiningClasses/DefiningClasses/Program.cs
+++ DefiningClasses/DefiningClasses/Program.cs
@@ -6,7 +6,15 @@ namespace DefiningClasses
     {
         static void Main(string[] args)
         {
-            
+            int n = int.Parse(Console.ReadLine());
+            Family family = new Family();
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                Person member = new Person(input[0], int.Parse(input[1]));
+                family.AddMember(member);
+            }
+            family.GetOldestMember();
         }
     }
 }
