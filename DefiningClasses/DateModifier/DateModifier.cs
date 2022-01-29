99e01cb3e4b15fcddc1daa251590214ccfbf42b2
Иксан <iksan168@abv.bg>
2022-01-29 16:55:06 +0200

    DefiningClasses-DateModifier-Solved

---
+++ DefiningClasses/DateModifier/DateModifier.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DateModifier
+{
+    class DateModifier
+    {
+        public void DateCalculator(string date1, string date2)
+        {
+            var dateOne = date1.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var year = dateOne[0];
+            var month = dateOne[1];
+            var day = dateOne[2];
+            DateTime calendar1 = new DateTime(year, month, day);
+            var dateTwo = date2.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var year2 = dateTwo[0];
+            var month2 = dateTwo[1];
+            var day2 = dateTwo[2];
+            DateTime calendar2 = new DateTime(year2, month2, day2);
+            var result = calendar2.Subtract(calendar1);
+            Console.WriteLine(Math.Abs(result.TotalDays));
+        }
+    }
+}
---
+++ DefiningClasses/DateModifier/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DateModifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateModifier date = new DateModifier();
+            var date1 = Console.ReadLine();
+            var date2 = Console.ReadLine();
+            date.DateCalculator(date1, date2);
+        }
+    }
+}
