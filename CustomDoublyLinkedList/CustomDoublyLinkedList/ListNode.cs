169bcc7e6ed5c2d0738c7b162b958edeaf88196e
Иксан <iksan168@abv.bg>
2022-02-02 12:59:34 +0200

    CustomDoublyLinkedList-Made

---
+++ CustomDoublyLinkedList/CustomDoublyLinkedList/ListNode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomDoublyLinkedList
+{
+    public class ListNode
+    {
+        public int Value { get; set; }
+        public int NextNode { get; set; }
+        public int PreviousNode { get; set; }
+    }
+}
---
+++ CustomDoublyLinkedList/CustomDoublyLinkedList/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CustomDoublyLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoublyLinkedList doubly = new DoublyLinkedList();
+            doubly.AddFirst(1);
+            doubly.AddFirst(2);
+            doubly.AddFirst(3);
+            Console.WriteLine($"Current state");
+            doubly.ForEach(n => Console.WriteLine(n));
+            Console.WriteLine($"----------------");
+            doubly.AddLast(1);
+            doubly.AddLast(2);
+            doubly.AddLast(3);
+            Console.WriteLine($"Current state");
+            doubly.ForEach(n => Console.WriteLine(n));
+            Console.WriteLine($"----------------");
+            doubly.RemoveFirst();
+            Console.WriteLine($"Current state");
+            doubly.ForEach(n => Console.WriteLine(n));
+            Console.WriteLine($"----------------");
+            doubly.RemoveLast();
+            Console.WriteLine($"Current state");
+            doubly.ForEach(n => Console.WriteLine(n));
+            Console.WriteLine($"----------------");
+        }
+    }
+}
