d20b22df1c3bff24275b288dbf8225717cd7b21f
Иксан <iksan168@abv.bg>
2022-02-05 18:10:10 +0200

    Generics

---
+++ Generics/GenericSwapMethodInteger/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace GenericSwapMethodInteger
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Swap<int> swapper = new Swap<int>();
+            for (int i = 0; i < n; i++)
+            {
+                var input = int.Parse(Console.ReadLine());
+                swapper.Swapper.Add(input);
+            }
+            var indexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            swapper.IndexSwapper<int>(indexes[0], indexes[1]);
+        }
+    }
+}
---
+++ Generics/GenericSwapMethodInteger/Swap.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericSwapMethodInteger
+{
+    public class Swap<T>
+    {
+        List<T> swapper = new List<T>();
+        public List<T> Swapper { get { return swapper; } set { this.swapper = value; } }
+        public void IndexSwapper<T>(int indexOne, int indexTwo)
+        {
+            var container = Swapper[indexOne];
+            Swapper[indexOne] = Swapper[indexTwo];
+            Swapper[indexTwo] = container;
+            foreach (var item in Swapper)
+            {
+                Console.WriteLine($"System.Int32: {item}");
+            }
+        }
+    }
+}
