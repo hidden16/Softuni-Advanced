f1af31ef9f52c4188fbddc7c020b8e1dd57ab2aa
Иксан <iksan168@abv.bg>
2022-02-05 17:41:00 +0200

    Generics

---
+++ Generics/GenericScale/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            EqualityScale<int> equality = new EqualityScale<int>(5, 3);
+            Console.WriteLine(equality.AreEqual()); 
+        }
+    }
+}
