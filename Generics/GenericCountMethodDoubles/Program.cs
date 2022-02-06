0b2e60ed748be564ae42f5b333950f13e28e0951
Иксан <iksan168@abv.bg>
2022-02-06 16:53:25 +0200

    Generics

---
+++ Generics/GenericCountMethodDoubles/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace GenericCountMethodDoubles
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<double> genericList = new List<double>();
+            for (int i = 0; i < n; i++)
+            {
+                var input = double.Parse(Console.ReadLine());
+                genericList.Add(input);
+            }
+            double comparingElements = double.Parse(Console.ReadLine());
+            Console.WriteLine(Compare(genericList, comparingElements));
+        }
+        public static double Compare<T>(List<T> list, T element)
+           where T : IComparable<T>
+        {
+            var counter = 0;
+            foreach (var generic in list)
+            {
+                if (generic.CompareTo(element) > 0)
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+    }
+}
---
+++ Generics/GenericCountMethodStrings/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace GenericCountMethodStrings
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> genericList = new List<string>();
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine();
+                genericList.Add(input);
+            }
+            string comparingElements = Console.ReadLine();
+            Console.WriteLine(Compare(genericList, comparingElements)) ;
+        }
+        public static int Compare<T>(List<T> list, T element)
+            where T : IComparable<T>
+        {
+            var counter = 0;
+            foreach (var generic in list)
+            {
+                if (generic.CompareTo(element) > 0)
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+    }
+}
