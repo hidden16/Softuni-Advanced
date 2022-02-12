f1fafd24d03ccf693fd1750632afc6ff4e262030
Иксан <iksan168@abv.bg>
2022-02-12 13:28:44 +0200

    IteratorsAndComparators

---
+++ IteratorsAndComparators/Library/Library/LibraryIterator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace IteratorsAndComparators
+{
+    public class LibraryIterator : IEnumerator<Book>
+    {
+        private List<Book> books;
+        private int currIndex;
+        public LibraryIterator(List<Book> books)
+        {
+            this.books = books;
+            currIndex = -1;
+        }
+        public Book Current => books[currIndex];
+        public bool MoveNext()
+        {
+            currIndex++;
+            return currIndex < books.Count;
+        }
+        public void Dispose() { }
+        public void Reset() { }
+        object IEnumerator.Current => Current; // legacy
+    }
+}
