74342c6e7dc9d909968d1b160d57ccfdf481626f
Иксан <iksan168@abv.bg>
2022-02-15 16:00:18 +0200

    ExamPreparation

---
+++ ExamPreparation/SuperMario/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+
+namespace SuperMario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var lives = int.Parse(Console.ReadLine());
+            var matrixRows = int.Parse(Console.ReadLine());
+            char[][] matrix = new char[matrixRows][];
+            var marioRow = 0;
+            var marioCol = 0;
+            for (int row = 0; row < matrix.Length; row++)
+            {
+                char[] input = Console.ReadLine().ToCharArray();
+                matrix[row] = new char[input.Length];
+                for (int col = 0; col < matrix[row].Length; col++)
+                {
+                    matrix[row][col] = input[col];
+                    if (matrix[row][col] == 'M')
+                    {
+                        marioRow = row;
+                        marioCol = col;
+                    }
+                }
+            }
+            while (true)
+            {
+                matrix[marioRow][marioCol] = '-';
+                var movement = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                var bowserRow = int.Parse(movement[1]);
+                var bowserCol = int.Parse(movement[2]);
+                matrix[bowserRow][bowserCol] = 'B';
+                lives--;
+                if (movement[0] == "W")
+                {
+                    if (IsInRange(matrix, marioRow - 1, marioCol))
+                    {
+                        marioRow--;
+                        if (matrix[marioRow][marioCol] == 'B')
+                        {
+                            lives -= 2;
+
+                        }
+                        else if (matrix[marioRow][marioCol] == 'P')
+                        {
+                            matrix[marioRow][marioCol] = '-';
+                            break;
+                        }
+
+                    }
+                }
+                else if (movement[0] == "S")
+                {
+                    if (IsInRange(matrix, marioRow + 1, marioCol))
+                    {
+                        marioRow++;
+                        if (matrix[marioRow][marioCol] == 'B')
+                        {
+                            lives -= 2;
+
+                        }
+                        else if (matrix[marioRow][marioCol] == 'P')
+                        {
+                            matrix[marioRow][marioCol] = '-';
+                            break;
+                        }
+                    }
+                }
+                else if (movement[0] == "A")
+                {
+                    if (IsInRange(matrix, marioRow, marioCol - 1))
+                    {
+                        marioCol--;
+                        if (matrix[marioRow][marioCol] == 'B')
+                        {
+                            lives -= 2;
+
+                        }
+                        else if (matrix[marioRow][marioCol] == 'P')
+                        {
+                            matrix[marioRow][marioCol] = '-';
+                            break;
+                        }
+                    }
+                }
+                else if (movement[0] == "D")
+                {
+                    if (IsInRange(matrix, marioRow, marioCol + 1))
+                    {
+                        marioCol++;
+                        if (matrix[marioRow][marioCol] == 'B')
+                        {
+                            lives -= 2;
+
+                        }
+                        else if (matrix[marioRow][marioCol] == 'P')
+                        {
+                            matrix[marioRow][marioCol] = '-';
+                            break;
+                        }
+                    }
+                }
+
+                if (lives <= 0)
+                {
+                    matrix[marioRow][marioCol] = 'X';
+                    Console.WriteLine($"Mario died at {marioRow};{marioCol}.");
+                    PrintMatrix(matrix, marioRow, marioCol);
+                    return;
+                }
+            }
+            Console.WriteLine($"Mario has successfully saved the princess! Lives left: {lives}");
+            PrintMatrix(matrix, marioRow, marioCol);
+        }
+        public static bool IsInRange(char[][] matrix, int row, int col)
+        {
+            if (row >= 0 && row < matrix.Length && col >= 0 && col < matrix[row].Length)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static void PrintMatrix(char[][] matrix, int row, int col)
+        {
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    Console.Write($"{matrix[i][j]}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ ExamPreparation/WarmWinter/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WarmWinter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input1 = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var input2 = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            Stack<int> hats = new Stack<int>(input1);
+            Queue<int> scarfs = new Queue<int>(input2);
+            List<int> set = new List<int>();
+            while (hats.Count != 0 && scarfs.Count != 0)
+            {
+                if (hats.Peek() > scarfs.Peek())
+                {
+                    var calc = hats.Pop() + scarfs.Dequeue();
+                    set.Add(calc);
+                }
+                else if (hats.Peek() < scarfs.Peek())
+                {
+                    hats.Pop();
+                    continue;
+                }
+                else if (hats.Peek() == scarfs.Peek())
+                {
+                    scarfs.Dequeue();
+                    hats.Push(hats.Pop() + 1);
+                }
+            }
+            var maxSet = set.OrderByDescending(x => x).First();
+            Console.WriteLine($"The most expensive set is: {maxSet}");
+            Console.WriteLine(String.Join(" ", set));
+        }
+    }
+}
