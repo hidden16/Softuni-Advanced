67e9ab1200aabe77c54328834b4a6b8f0b1fbcec
Иксан <iksan168@abv.bg>
2022-02-18 14:05:07 +0200

    ExamPreparation

---
+++ ExamPreparation/Scheduling/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Scheduling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var tasksInput = Console.ReadLine().Split(", ",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var threadsInput = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            Stack<int> tasks = new Stack<int>(tasksInput);
+            Queue<int> threads = new Queue<int>(threadsInput);
+            var taskToKill = int.Parse(Console.ReadLine());
+            while (true)
+            {
+                if (tasks.Peek() == taskToKill)
+                {
+                    tasks.Pop();
+                    Console.WriteLine($"Thread with value {threads.Peek()} killed task {taskToKill}");
+                    Console.WriteLine(string.Join(" ", threads));
+                    return;
+                }
+                else if (threads.Peek() >= tasks.Peek())
+                {
+                    threads.Dequeue();
+                    tasks.Pop();
+                }
+                else if (threads.Peek() < tasks.Peek())
+                {
+                    threads.Dequeue();
+                }
+            }
+        }
+    }
+}
