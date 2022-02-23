8f1f26d3fada295787ee314fe8a5e485bfeed3cf
Иксан <iksan168@abv.bg>
2022-02-23 20:50:42 +0200

    OOP-Exercises

---
+++ OOP/Restaurant/Cake.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        public Cake(string name) : base(name, 0, 0, 0)
+        {
+
+        }
+        public override double Grams => 250;
+        public override double Calories => 1000;
+        public override decimal Price => 5;
+    }
+}
---
+++ OOP/Restaurant/Coffee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        private const double coffeeMililiters = 50;
+        private const decimal coffeePrice = 3.50m;
+        public Coffee(string name, double caffeine):base(name,coffeePrice,coffeeMililiters)
+        {
+            Caffeine = caffeine;
+        }
+        public double Caffeine { get; set; }
+    }
+}
