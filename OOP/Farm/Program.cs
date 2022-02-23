7c202951d35c8f016bf8194e2e00e41bd92cb553
Иксан <iksan168@abv.bg>
2022-02-23 14:22:56 +0200

    Exercise-2-Solved

--- OOP/Farm/Program.cs
+++ OOP/Farm/Program.cs
@@ -6,9 +6,11 @@ namespace Farm
     {
         static void Main(string[] args)
         {
-            Dog dog = new Dog();
-            dog.Eat();
-            dog.Bark();
+            Puppy puppy = new Puppy();
+            puppy.Eat();
+            puppy.Bark();
+            puppy.Weep();
+
         }
     }
 }
