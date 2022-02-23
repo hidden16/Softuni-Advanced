6bcd023edcfa17c3d5d7965beedbd2765d65c338
Иксан <iksan168@abv.bg>
2022-02-23 14:30:28 +0200

    CustomRandomList-Solved

---
+++ OOP/CustomRandomList/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList rand = new RandomList();
+            rand.Add("1");
+            rand.Add("2");
+            rand.Add("3");
+            rand.Add("4");
+            rand.Add("5");
+
+            Console.WriteLine(rand.RandomString());
+            Console.WriteLine(rand.RandomString());
+            Console.WriteLine(rand.RandomString());
+            Console.WriteLine(rand.RandomString());
+            Console.WriteLine(rand.RandomString());
+        }
+    }
+}
---
+++ OOP/CustomRandomList/RandomList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        Random rand = new Random();
+        public string RandomString()
+        {
+            if (Count == 0) return null;
+            
+            var index = rand.Next(0, Count);
+            var element = this[index];
+            RemoveAt(index);
+            return element;
+        }
+    }
+}
