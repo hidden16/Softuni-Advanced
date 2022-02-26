2a1bd8c73d3dc1c32d199bc6c3e13550fed00853
Иксан <iksan168@abv.bg>
2022-02-26 13:43:37 +0200

    Encapsulation-Lab-Solved

--- OOP/Encapsulation/SortPersonsByNameAndAge/Program.cs
+++ OOP/Encapsulation/SortPersonsByNameAndAge/Program.cs
@@ -8,21 +8,22 @@ namespace PersonsInfo
     {
         static void Main(string[] args)
         {
-            var lines = int.Parse(Console.ReadLine());
-            var persons = new List<Person>();
-            for (int i = 0; i < lines; i++)
+            Team team = new Team("SoftUni");
+            Person p1 = new Person("Iksan", "Ismet", 32, 4000);
+            Person p2 = new Person("Ivan", "Grobarq", 17, 4000);
+            Person p3 = new Person("Georgi", "Bugnatiq", 22, 4000);
+            Person p4 = new Person("Sasho", "Boksiora", 26, 4000);
+            List<Person> persons = new List<Person>();
+            persons.Add(p1);
+            persons.Add(p2);
+            persons.Add(p3);
+            persons.Add(p4);
+            foreach (Person person in persons)
             {
-                var cmdArgs = Console.ReadLine().Split();
-                var person = new Person(cmdArgs[0],
-                                        cmdArgs[1],
-                                        int.Parse(cmdArgs[2]),
-                                        decimal.Parse(cmdArgs[3]));
-
-                persons.Add(person);
+                team.AddPlayer(person);
             }
-            var parcentage = decimal.Parse(Console.ReadLine());
-            persons.ForEach(p => p.IncreaseSalary(parcentage));
-            persons.ForEach(p => Console.WriteLine(p.ToString()));
+            Console.WriteLine($"First team has {team.FirstTeam.Count} players");
+            Console.WriteLine($"Reserve team has {team.ReserveTeam.Count} players");
         }
     }
 }
---
+++ OOP/Encapsulation/SortPersonsByNameAndAge/Team.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+        public Team(string name)
+        {
+            this.name = name;
+            firstTeam = new List<Person>();
+            reserveTeam = new List<Person>();
+        }
+        public IReadOnlyCollection<Person> FirstTeam { get { return firstTeam; } }
+        public IReadOnlyCollection<Person> ReserveTeam { get { return reserveTeam; } }
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 30)
+            {
+                firstTeam.Add(person);
+            }
+            else
+            {
+                reserveTeam.Add(person);
+            }
+        }
+    }
+}
