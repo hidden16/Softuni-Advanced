0215b1d5903bbbcebe943d6ab85c499d613ca1f4
Иксан <iksan168@abv.bg>
2022-02-26 13:32:00 +0200

    Encapsulation-Lab-3-Solved

--- OOP/Encapsulation/SortPersonsByNameAndAge/Person.cs
+++ OOP/Encapsulation/SortPersonsByNameAndAge/Person.cs
@@ -18,10 +18,65 @@ namespace PersonsInfo
             Salary = salary;
         }
 
-        public string FirstName { get { return firstName; } private set { firstName = value; } }
-        public string LastName { get { return lastName; } private set { lastName = value; } }
-        public int Age { get { return age; } private set { age = value; } }
-        public decimal Salary { get { return salary; } private set { salary = value; } }
+        public string FirstName
+        {
+            get
+            { 
+                return firstName;
+            } 
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                }
+                firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            } 
+            private set 
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }
+                lastName = value;
+            } 
+        }
+        public int Age
+        { 
+            get
+            { return age;
+            }
+            private set 
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+                age = value;
+            }
+        }
+        public decimal Salary
+        {
+            get 
+            { 
+                return salary;
+            }
+            private set 
+            {
+                if (value < 650m)
+                {
+                    throw new ArgumentException("Salary cannot be less than 650 leva!");
+                }
+                salary = value;
+            } 
+        }
         public void IncreaseSalary(decimal percentage)
         {
             if (Age > 30)
