15b798163e2298107322a1e89ebeba2075a19e72
Иксан <iksan168@abv.bg>
2022-02-26 13:25:55 +0200

    OOP-LabEx2

--- OOP/Encapsulation/SortPersonsByNameAndAge/Person.cs
+++ OOP/Encapsulation/SortPersonsByNameAndAge/Person.cs
@@ -9,19 +9,33 @@ namespace PersonsInfo
         private string firstName;
         private string lastName;
         private int age;
-        public Person(string firstName, string lastName, int age)
+        private decimal salary;
+        public Person(string firstName, string lastName, int age,decimal salary)
         {
             FirstName = firstName;
             LastName = lastName;
             Age = age;
+            Salary = salary;
         }
 
         public string FirstName { get { return firstName; } private set { firstName = value; } }
         public string LastName { get { return lastName; } private set { lastName = value; } }
         public int Age { get { return age; } private set { age = value; } }
+        public decimal Salary { get { return salary; } private set { salary = value; } }
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (Age > 30)
+            {
+                Salary += Salary * percentage / 100;
+            }
+            else
+            {
+                Salary += Salary * percentage / 200;
+            }
+        }
         public override string ToString()
         {
-            return $"{FirstName} {LastName} is {Age} years old.";
+            return $"{FirstName} {LastName} receives {Salary:f2} leva.";
         }
     }
 }
