cdd6348312629e8ee7748219c9c241709a0eda0b
Иксан <iksan168@abv.bg>
2022-03-09 17:50:13 +0200

    ExceptionsAndErrorHandilng-Exercise-1-Solved

---
+++ OOP/ExceptionsAndErrorHandling/SquareRoot/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SquareRoot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int n = int.Parse(Console.ReadLine());
+                if (n < 0)
+                {
+                    throw new ArgumentException();
+                }
+                Console.WriteLine(Math.Sqrt(n));
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Invalid number.");
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}
