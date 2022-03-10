09110cb3b4ad0412bda2a1d2254f3379029e1eeb
Иксан <iksan168@abv.bg>
2022-03-10 13:21:56 +0200

    Exceptions

---
+++ OOP/ExceptionsAndErrorHandling/SumOfIntegers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace SumOfIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(" ").ToArray();
+            var sum = 0;
+            var number = string.Empty;
+                foreach (var item in input)
+                {
+                    try
+                    {
+                        number = item;
+                        sum += int.Parse(number);
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine($"The element '{item}' is in wrong format!");
+                    }
+                    catch (OverflowException)
+                    {
+                        Console.WriteLine($"The element '{item}' is out of range!");
+                    }
+                    finally
+                    {
+                        Console.WriteLine($"Element '{item}' processed - current sum: {sum}");
+                    }
+                }
+                Console.WriteLine($"The total sum of all integers is: {sum}");
+        }
+    }
+}
