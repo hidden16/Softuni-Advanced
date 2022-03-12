0063840f2929c2e27fae0820e84505fc08460d9b
Иксан <iksan168@abv.bg>
2022-03-12 17:25:13 +0200

    SOLID-Lab-1-Refactored

---
+++ OOP/SOLID/P01.Stream_Progress/Interfaces/IStreamable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01.Stream_Progress
+{
+    public interface IStreamable : ISource
+    {
+        public int BytesSent { get; }
+    }
+}
---
+++ OOP/SOLID/P01.Stream_Progress/Streams/StreamProgressor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace P01.Stream_Progress.Streams
+{
+    public abstract class StreamProgressor
+    {
+        private IStreamable file;
+
+        // If we want to stream a music file, we can't
+        public StreamProgressor(IStreamable file)
+        {
+            this.file = file;
+        }
+
+        public virtual int CalculateCurrentPercent()
+        {
+            return (this.file.BytesSent * 100) / this.file.Length;
+        }
+    }
+}
