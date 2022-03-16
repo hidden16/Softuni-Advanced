2e7b4eca0a7531bc815eaaebad74744f234bad01
Иксан <iksan168@abv.bg>
2022-03-16 12:20:15 +0200

    Attributes-Lab-5-Solved

---
+++ OOP/ReflectionAndAttributes/AuthorProblem/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    [Author("Victor")]
+    public class StartUp
+    {
+        [AuthorAttribute("George")]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
