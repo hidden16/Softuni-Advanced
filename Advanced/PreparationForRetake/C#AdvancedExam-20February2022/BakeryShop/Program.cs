483e1c61bc1219e9fa828a3af64cb92ff8bc0654
Иксан <iksan168@abv.bg>
2022-04-10 20:47:09 +0300

    ExamPrep

---
+++ Advanced/PreparationForRetake/C#AdvancedExam-20February2022/BakeryShop/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BakeryShop
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // water calculation (16.8 + 25.2 = 42; (16.8 * 100)/42 = 40% water)  --- 16.8 (Water) 25.2 (flour)
+            var waterInput = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
+            var flourInput = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
+
+            Queue<double> water = new Queue<double>(waterInput);
+            Stack<double> flour = new Stack<double>(flourInput);
+            Dictionary<string, int> bakedProducts = new Dictionary<string, int>
+            {
+                {"Croissant",0 },
+                {"Muffin",0 },
+                {"Baguette",0 },
+                {"Bagel",0 }
+            };
+
+            while (water.Count > 0 && flour.Count > 0)
+            {
+                var sum = water.Peek() + flour.Peek();
+                var calculation = (water.Peek() * 100) / sum;
+                if (calculation == 50)
+                {
+                    bakedProducts["Croissant"]++;
+                    water.Dequeue();
+                    flour.Pop();
+                }
+                else if (calculation == 40)
+                {
+                    bakedProducts["Muffin"]++;
+                    water.Dequeue();
+                    flour.Pop();
+                }
+                else if (calculation == 30)
+                {
+                    bakedProducts["Baguette"]++;
+                    water.Dequeue();
+                    flour.Pop();
+                }
+                else if (calculation == 20)
+                {
+                    bakedProducts["Bagel"]++;
+                    water.Dequeue();
+                    flour.Pop();
+                }
+                else
+                {
+                    var excess = flour.Peek() - water.Peek();
+                    flour.Push(flour.Pop() - excess);
+                    bakedProducts["Croissant"]++;
+                    flour.Pop();
+                    water.Dequeue();
+                    flour.Push(excess);
+                }
+            }
+            foreach (var product in bakedProducts.OrderByDescending(x=>x.Value).ThenBy(x=>x.Key).Where(x=>x.Value > 0))
+            {
+                Console.WriteLine($"{product.Key}: {product.Value}");
+            }
+            if (water.Count > 0)
+            {
+                Console.WriteLine($"Water left: {string.Join(", ", water)}");
+            }
+            else
+            {
+                Console.WriteLine($"Water left: None");
+            }
+
+            if (flour.Count > 0)
+            {
+                Console.WriteLine($"Flour left: {string.Join(", ", flour)}");
+            }
+            else
+            {
+                Console.WriteLine($"Flour left: None");
+            }
+        }
+    }
+}
---
+++ Advanced/PreparationForRetake/C#AdvancedExam-20February2022/BeaverAtWork/BeaverAtWork/Program.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BeaverAtWork
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[n, n];
+
+            var currRow = 0;
+            var currCol = 0;
+            int numberOfBranches = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] input = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+                    if (matrix[row, col] == 'B')
+                    {
+                        currRow = row;
+                        currCol = col;
+                    }
+                    if (char.IsLower(matrix[row, col]))
+                    {
+                        numberOfBranches++;
+                    }
+                }
+            }
+            var commands = Console.ReadLine();
+            List<char> collectedBranches = new List<char>();
+            while (commands != "end")
+            {
+                if (commands == "up" && IsInRange(matrix, currRow - 1, currCol))
+                {
+                    matrix[currRow, currCol] = '-';
+                    currRow--;
+                    if (char.IsLower(matrix[currRow, currCol]))
+                    {
+                        collectedBranches.Add(matrix[currRow,currCol]);
+                        matrix[currRow, currCol] = 'B';
+                        numberOfBranches--;
+                    }
+                    else if (matrix[currRow, currCol] == 'F')
+                    {
+                        matrix[currRow, currCol] = '-';
+                        if (currRow == 0)
+                        {
+                            currRow = matrix.GetLength(0) - 1;
+                            matrix[currRow, currCol] = 'B';
+                        }
+                        else
+                        {
+                            currRow = 0;
+                            matrix[currRow, currCol] = 'B';
+                        }
+                    }
+                    else
+                    {
+                        matrix[currRow, currCol] = 'B';
+                    }
+                }
+                else if (commands == "down" && IsInRange(matrix, currRow + 1, currCol))
+                {
+                    matrix[currRow, currCol] = '-';
+                    currRow++;
+                    if (char.IsLower(matrix[currRow, currCol]))
+                    {
+                        collectedBranches.Add(matrix[currRow, currCol]);
+                        matrix[currRow, currCol] = 'B';
+
+                        numberOfBranches--;
+                    }
+                    else if (matrix[currRow, currCol] == 'F')
+                    {
+                        matrix[currRow, currCol] = '-';
+
+                        if (currRow == matrix.GetLength(0) - 1)
+                        {
+                            currRow = 0;
+                            matrix[currRow, currCol] = 'B';
+                        }
+                        else
+                        {
+                            currRow = matrix.GetLength(0) - 1;
+                            matrix[currRow, currCol] = 'B';
+                        }
+                    }
+                    else
+                    {
+                        matrix[currRow, currCol] = 'B';
+                    }
+                }
+                else if (commands == "left" && IsInRange(matrix, currRow, currCol - 1))
+                {
+                    matrix[currRow, currCol] = '-';
+                    currCol--;
+                    if (char.IsLower(matrix[currRow, currCol]))
+                    {
+                        collectedBranches.Add(matrix[currRow, currCol]);
+                        matrix[currRow, currCol] = 'B';
+
+                        numberOfBranches--;
+                    }
+                    else if (matrix[currRow, currCol] == 'F')
+                    {
+                        matrix[currRow, currCol] = '-';
+                        if (currCol == 0)
+                        {
+                            currCol = matrix.GetLength(1) - 1;
+                            matrix[currRow, currCol] = 'B';
+                        }
+                        else
+                        {
+                            currCol = 0;
+                            matrix[currRow, currCol] = 'B';
+                        }
+                    }
+                    else
+                    {
+                        matrix[currRow, currCol] = 'B';
+                    }
+                }
+                else if (commands == "right" && IsInRange(matrix, currRow, currCol + 1))
+                {
+                    matrix[currRow, currCol] = '-';
+                    currCol++;
+                    if (char.IsLower(matrix[currRow, currCol]))
+                    {
+                        collectedBranches.Add(matrix[currRow, currCol]);
+                        matrix[currRow, currCol] = 'B';
+
+                        numberOfBranches--;
+                    }
+                    else if (matrix[currRow, currCol] == 'F')
+                    {
+                        matrix[currRow, currCol] = '-';
+                        if (currCol == matrix.GetLength(1) - 1)
+                        {
+                            currCol = 0;
+                            matrix[currRow, currCol] = 'B';
+                        }
+                        else
+                        {
+                            currCol = matrix.GetLength(1) - 1;
+                            matrix[currRow, currCol] = 'B';
+                        }
+                    }
+                    else
+                    {
+                        matrix[currRow, currCol] = 'B';
+                    }
+                }
+                else
+                {
+                    if (collectedBranches.Count > 0)
+                    {
+                        collectedBranches.RemoveAt(collectedBranches.Count - 1);
+                    }
+                }
+                if (numberOfBranches == 0)
+                {
+                    break;
+                }
+                commands = Console.ReadLine();
+            }
+            if (numberOfBranches == 0)
+            {
+                Console.WriteLine($"The Beaver successfully collect {collectedBranches.Count} wood branches: {string.Join(", ", collectedBranches)}. ");
+            }
+            else
+            {
+                Console.WriteLine($"The Beaver failed to collect every wood branch. There are {numberOfBranches} branches left.");
+            }
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write($"{matrix[row,col]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+        public static bool IsInRange(char[,] matrix, int row, int col)
+        {
+            if (row >= 0 && row < matrix.GetLength(0) && col >= 0 && col < matrix.GetLength(1))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
